b82f51030d3b19b542b7792e51dfadd878d874d9
Stephen Thurman <thurman024@gmail.com>
2018-01-31 20:11:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  attr_accessor
+  attr_accessor :type
 
   def initialize(side1, side2, side3)
     @side1 = side1
@@ -8,7 +8,13 @@ class Triangle
   end
 
   def kind
-    
+    sides = [@side1, @side2, @side3].sort
+    if sides[0] + sides[1] <= sides[2]
+      raise TriangleError
+    elsif sides[0] < 0
+      raise TriangleError
+    elsif sides[0] == sides[1] || sides[1] == sides[2]
+
 end
 
 
